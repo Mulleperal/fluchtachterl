0d98e28201eecc352f1378ca8db2c0e50c12045b
Paul Meller <wi20b016@technikum-wien.at>
2022-11-15 19:19:42 +0100

    comit

--- vuejs/src/components/HelloWorld.vue
+++ vuejs/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>Welcome to Fluchtachterl</h1>
+    <div>Your favorite site to stock up your beverage supply.</div>
   </div>
 </template>
 
@@ -10,21 +11,3 @@ export default {
   props: {},
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
